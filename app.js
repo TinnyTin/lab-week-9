be1f70c0446f1771d3788510fae551b3aac3431b
Martin Lee <martinlee5159@gmail.com>
2021-03-10 18:39:23 -0800

    add required lab files

---
+++ app.js
@@ -0,0 +1,26 @@
+// Declarations
+const fingers = 5; 
+const online = true;
+
+let x = 1;
+let name = 'Martin';
+
+// If statement
+if (x === fingers){
+    console.log("they match")
+} else {
+    console.log("no luck")
+}
+
+// Testing the if conditional
+let x = 5;
+if (x === fingers){
+    console.log("they match")
+} else {
+    console.log("no luck")
+}
+
+// For loop that prints every letter of name variable
+for (let letter of name){
+    console.log(letter)
+}
